48e0f9059e0eca143f4196e7fef2f2d3f14b2587
jairo.rojas <jaro.rojas@avaldigitallabs.com>
2019-09-26 12:43:17 -0500

    :tada: Initial commit

---
+++ terraform.auto.tfvars
@@ -0,0 +1,1 @@
+tempvar = "tempvalue"
--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "aws_region" {
   type        = string
   description = "The region where resources will be deployed"
 }
+
+variable "tempvar" {
+  type        = string
+  description = "tempdescription"
+}
