6ea22d76939989bb16d8372dd5f51381d7608464
jairo.rojas <jaro.rojas@avaldigitallabs.com>
2019-09-30 21:50:26 -0500

    :sparkles: Initial Infrastructure

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket_policy" "s3_bucket_policy_to_access_from_cloudfront" {
-  bucket = "${aws_s3_bucket.front_bkt.id}"
+  bucket = "${aws_s3_bucket.front.id}"
   policy = "${data.aws_iam_policy_document.s3_policy_document_cloudfront.json}"
 }
