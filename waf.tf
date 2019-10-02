132d2e2dbbbc0dcdbfe3ab0c920d06bbb54ccfbd
jairo.rojas <jaro.rojas@avaldigitallabs.com>
2019-10-02 18:17:59 -0500

    :sparkles: Initial Infrastructure

--- waf.tf
+++ waf.tf
@@ -1,6 +1,6 @@
 resource "aws_waf_web_acl" "waf" {
-  name        = var.stack_id
-  metric_name = var.stack_id
+  name        = replace(var.stack_id, "-", "")
+  name        = replace(var.stack_id, "-", "")
 
   default_action {
     type = "ALLOW"
